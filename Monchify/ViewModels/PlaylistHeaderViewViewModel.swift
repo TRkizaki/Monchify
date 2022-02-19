17bc6ca812c357c2ea6590330b365456873f389b
TR Kizaki <trkizaki.web@gmail.com>
2022-02-20 01:56:57 +0400

    update function of playlist

---
+++ Monchify/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  Monchify
+//
+//  Created by DJ perrier  on 19/2/22.
+//
+
+import Foundation
---
+++ Monchify/Views/PlaylistHeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  PlaylistHeaderCollectionReusableView.swift
+//  Monchify
+//
+//  Created by DJ perrier  on 19/2/22.
+//
+
+import UIKit
+
+class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
+        
+}
