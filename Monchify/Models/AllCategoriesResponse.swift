53395522c5b213b072ec3f272bb4112624b10eff
TR Kizaki <trkizaki.web@gmail.com>
2022-02-23 14:04:21 +0400

    set up playlist on search

---
+++ Monchify/Models/AllCategoriesResponse.swift
@@ -0,0 +1,8 @@
+//
+//  AllCategoriesResponse.swift
+//  Monchify
+//
+//  Created by DJ perrier  on 21/2/22.
+//
+
+import Foundation
---
+++ Monchify/ViewModels/CategoryCollectionViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryCollectionViewCellViewModel.swift
+//  Monchify
+//
+//  Created by DJ perrier  on 22/2/22.
+//
+
+import Foundation
